14b64525682b6f5b364d913108ee05a48a60f331
Khoa Nguyen <60150482+khoainho@users.noreply.github.com>
2024-03-06 23:25:20 -0600

    Add Discord commands

---
+++ commands/commands.py
@@ -0,0 +1,21 @@
+import requests
+import yaml
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+APPLICATION_ID = os.getenv('APPLICATION_ID')
+URL = f"https://discord.com/api/v9/applications/{APPLICATION_ID}/commands"
+
+with open("commands/commands.yaml", "r") as file:
+    yaml_content = file.read()
+
+commands = yaml.safe_load(yaml_content)
+headers = {"Authorization": f"Bot {DISCORD_TOKEN}", "Content-Type": "application/json"}
+
+# Send the POST request for each command
+for command in commands:
+    response = requests.post(URL, json=command, headers=headers)
+    command_name = command["name"]
+    print(f"Command {command_name} created: {response.status_code}")
