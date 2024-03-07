23e68e90d1e2fa3c5dd735950ab46d7d2aa79619
Khoa Nguyen <60150482+khoainho@users.noreply.github.com>
2024-03-06 23:12:21 -0600

    App init

---
+++ test/khoala.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Khoala from '../lib/khoala-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/khoala-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Khoala.KhoalaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
